882e90900cc85d7afab82098905da155f42b97c3
HubertSienicki <businesssienicki@gmail.com>
2022-12-16 22:36:02 +0100

    Implemented and tested simple key:value communication with a basic client

--- src/DatabaseNode.java
+++ src/DatabaseNode.java
@@ -7,11 +7,11 @@ import java.net.Socket;
 
 public class DatabaseNode {
     private final String NODE_TCP_PORT;
-    private String NODE_TCP_ADDRESS;
-    private String PREV_NODE_TCP_PORT;
-    private String PREV_NOTE_TCP_ADDRESS;
-    private int key;
-    private int value;
+    private final String NODE_TCP_ADDRESS;
+    private final String PREV_NODE_TCP_PORT;
+    private final String PREV_NOTE_TCP_ADDRESS;
+    private final int key;
+    private final int value;
     private ServerSocket nodeServerSocket;
     private Socket nodeSocket;
     private static int nodeID = 0;
@@ -33,20 +33,50 @@ public class DatabaseNode {
             System.out.println("New node created \n Listening for connections...");
             nodeSocket = nodeServerSocket.accept();
 
+            System.out.println();
             System.out.println("Connected!");
 
-            PrintWriter output = new PrintWriter(nodeSocket.getOutputStream());
-            BufferedReader input = new BufferedReader(new InputStreamReader(nodeSocket.getInputStream()));
-
-            String inputLine, outputLine;
-
-            while((inputLine = input.readLine()) != null){
-                System.out.println(inputLine);
-            }
+            manageInput();
 
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+    private void manageInput() throws IOException {
+        PrintWriter output = new PrintWriter(nodeSocket.getOutputStream(), true);
+        BufferedReader input = new BufferedReader(new InputStreamReader(nodeSocket.getInputStream()));
+
+        String inputLine;
+
+
+        while((inputLine = input.readLine()) != null){
+
+            System.out.println("Message recieved: ");
+            System.out.println(inputLine);
+            System.out.println();
+
+            String[] inputValues = inputLine.split(" ");
+
+            if (inputValues[0].equals("get-value")) {
+                sendMessage(output, getValue(Integer.parseInt(inputValues[1])));
+            }
+        }
+    }
+
+    private String getValue(int key){
+        if(key == this.key){
+            System.out.println("The key is correct!");
+            return String.valueOf(this.value);
+        }else{
+            return "ERROR: The key is not correct!";
+        }
+    }
+
+    private void sendMessage(PrintWriter output, String message){
+        System.out.println("Sending response...");
+        System.out.println();
+        output.println(message);
+        System.out.println("Message sent!");
     }
 
 }
